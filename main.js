bc8502796e86d58f08e6cc4ffeec5baef40a7d78
Harsheek Thanki <harsheekthanki@gmail.com>
2019-04-25 22:09:05 +0100

    Next slide, previous slide, auto, and keyMove functions added to JS file. media query added to CSS.

---
+++ main.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+function nextSlide() {
+    const current = document.querySelector('.current');
+    //remove current class    
+    current.classList.remove('current');
+    if (current.nextElementSibling){
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    }
+
+    setTimeout(() => current.classList.remove('current'));
+}
+
+
+function prevSlide() {
+    const current = document.querySelector('.current');
+
+    //remove current class
+    current.classList.remove('current');
+
+    if (current.previousElementSibling){
+        current.previousElementSibling.classList.add('current');
+    } else {
+        slides[slides.length-1].classList.add('current');
+    }
+
+    setTimeout(() => current.classList.remove('current'));
+}
+
+function keyMove(e) {
+    if (e.keyCode == "39") {
+        nextSlide();
+    }
+    if (e.keyCode == "37") {
+        prevSlide();
+    }
+}
+
+if(auto) {
+    slideInterval = setInterval(nextSlide, intervalTime );
+}
+
+
+window.addEventListener('keydown', keyMove);
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
