37f1c1ded179f213c838f42e66c7352184f5a5f9
Harsheek Thanki <harsheekthanki@gmail.com>
2019-04-27 19:31:44 +0100

    navbar added, media queries for navbar added. Pause button added to HTML, styled in CSS and code clean up in main.js

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
+const pause = document.querySelector('#pause');
 
-const auto = false;
-const intervalTime = 8000;
-// let slideInterval;
+let paused = false;
+const intervalTime = 1000;
+pause.innerHTML = 'Pause Slideshow';
 
 function nextSlide() {
     const current = document.querySelector('.current');
     //remove current class    
     current.classList.remove('current');
+
+    //add current class to the next slide
     if (current.nextElementSibling){
         current.nextElementSibling.classList.add('current');
     } else {
         slides[0].classList.add('current');
     }
-
-    // setTimeout(() => current.classList.remove('current'));
 }
 
 
 function prevSlide() {
     const current = document.querySelector('.current');
-
-    //remove current class
+    //remove current class from current element
     current.classList.remove('current');
 
+    //add current class to the next slide
     if (current.previousElementSibling){
         current.previousElementSibling.classList.add('current');
     } else {
         slides[slides.length-1].classList.add('current');
     }
-
-    // setTimeout(() => current.classList.remove('current'));
 }
 
 function keyMove(e) {
@@ -44,15 +43,25 @@ function keyMove(e) {
     }
 }
 
-if(auto) {
-    setInterval(nextSlide, intervalTime );
+//play/pause slideshow
+function slideControl() {
+    //if paused, start slideshow, change paused status and change button wording
+    if(paused) {
+        setInterval(nextSlide, intervalTime);
+        paused = false;
+        pause.innerHTML = 'Pause Slideshow';
+    } else {
+    //if not paused, stop slideshow, change paused status and change button wording
+        paused = true;
+        pause.innerHTML = 'Play Slideshow';        
+    }
 }
 
-//play/pause button function
-
-
+//Add EventListeners to variables storing DOM elements/hook up functions above to the DOM elements
 
 window.addEventListener('keydown', keyMove);
-
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+pause.addEventListener('click', slideControl);
+
+// slideControl();
